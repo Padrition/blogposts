149f0066bae583b29752392341292c8857d48a9b
Dmitrii Titarenko <padrition@gmail.com>
2024-11-01 23:06:22 +0100

    (app setup)
    Setup of axum application; containerized axum backend app and docker compose file to build backend with postgresql container

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Serialize,Deserialize};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("->> SERVER STARTED\n");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str{
+    "Hello, World!"
+}
+
+async fn create_user(
+    Json(payload): Json<CreateUser>,
+)-> (StatusCode, Json<User>){
+    let user = User{
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser{
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User{
+    id: u64,
+    username: String,
+}
